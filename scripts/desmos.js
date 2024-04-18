9e0c97d9a67b59de0db66322430686c71f4b85e6
AstridWehlitz <astridwehlitz@protonmail.com>
2024-04-17 22:01:45 -0600

    Wrote Javascript to Write Javascript to easily add a bunch of labeled points from a spreadsheet to a desmos graph

--- scripts/desmos.js
+++ scripts/desmos.js
@@ -1,3 +1,37 @@
-var elt = document.getElementById('calculator');
-var calculator = Desmos.GraphingCalculator(elt);
-calculator.setExpression({id:'graph1', latex:'y=x^2'});
+
+
+function generateDesmosInjection() {
+    var elt = document.getElementById("pastein")
+    var text = elt.value
+
+    var inlines = text.split("\n")
+    for (i = 0; i < inlines.length; i++) {
+        inlines[i] = inlines[i].split("	")
+    }
+
+    var xs = []
+    var ys = []
+    var ls = []
+
+    for (i = 0; i < inlines.length; i++) {
+        xs.push(inlines[i][0]);
+        ys.push(inlines[i][1]);
+        ls.push("'"+inlines[i][2]+"'");
+    }
+
+    var lines = []
+    lines.push("state = Calc.getState();")
+    lines.push("pointsx = [" + xs.toString()+"];")
+    lines.push("pointsy = [" + ys.toString()+"];")
+    lines.push("labels = [" + ls.toString()+"];")
+    lines.push("for (i = 0; i < pointsx.length; i++) {")
+    lines.push("state.expressions.list.push({type:'expression',latex:'('+pointsx[i]+','+pointsy[i]+')',label:labels[i],showLabel:true});}")
+    lines.push("Calc.setState(state);")
+
+    var out = ""
+    for (i = 0; i < lines.length; i++) {
+        out+=lines[i]+"\n"
+    }
+
+    document.getElementById("output").textContent = out
+}
